8ad507ca185b5c8e2eaa52141a26411ebc966e39
Nimodh Perera <73911823+Nimodh@users.noreply.github.com>
2021-06-26 19:12:06 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+    <h1>Web Dev with Javascript and React.js</h1>
+    <p>Topics to be covered are intro to programming in javascript, introduction to html, foundational knowledge in react.js, hands on project</p>
+    </div>
+  );
+}
+
+export default Info;
